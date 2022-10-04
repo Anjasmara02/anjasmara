940827142bdc8289e8e198b7a55b221ad3d8eadf
Anjasmara02 <114128325+Anjasmara02@users.noreply.github.com>
2022-10-04 22:23:03 +0700

    Add files via upload
    POSTTEST3

---
+++ POSTTEST3/javascript.js
@@ -0,0 +1,11 @@
+alert("welcome to my website");
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+document.getElementById('listener').addEventListener('mouseover',myFunction)
+function myFunction(){
+    alert('teklik')
+}
+        
